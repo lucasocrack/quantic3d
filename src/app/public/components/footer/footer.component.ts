d5330a94933005b52bf9354cc176a10c6bbb44e0
Alef Alexandre Silva Corrêa <102564597+Alef1412@users.noreply.github.com>
2024-08-20 23:00:57 -0300

    add float whats app icon

--- src/app/public/components/footer/footer.component.ts
+++ src/app/public/components/footer/footer.component.ts
@@ -1,27 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, OnInit, Inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { CustomizerSettingsService } from '../../../shared/components/customizer-settings/customizer-settings.service';
 import { MatIcon } from '@angular/material/icon';
 import { MatButton } from '@angular/material/button';
+import { CustomizerSettingsService } from 'src/app/shared/components/customizer-settings/customizer-settings.service';
+
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [RouterLink, MatIcon, MatButton],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.scss'
+  styleUrls: ['./footer.component.scss'] // Corrected from styleUrl to styleUrls
 })
-export class FooterComponent {
-  // isToggled
+export class FooterComponent implements OnInit {
   isToggled = false;
 
   constructor(
-    public customizerSettingsService: CustomizerSettingsService,
-    public themeService: CustomizerSettingsService
-  ) {
-    this.themeService.isToggled$.subscribe(isToggled => {
+      private el: ElementRef,
+      private renderer: Renderer2,
+      @Inject(CustomizerSettingsService) public customizerSettingsService: CustomizerSettingsService,
+      @Inject(CustomizerSettingsService) public themeService: CustomizerSettingsService
+    ) {}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      const quickMessage = this.el.nativeElement.querySelector('.quick-message');
+      if (quickMessage) {
+        this.renderer.setStyle(quickMessage, 'display', 'none'); // Corrected setStyle method
+      }
+    }, 9000);
+
+    this.themeService.isToggled$.subscribe((isToggled: boolean) => {
       this.isToggled = isToggled;
     });
   }
-
 }
