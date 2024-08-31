4341b7ec7d652fa850b4ba9a89b9bd925035f34f
Lucas Campos <bhlucascampos@gmail.com>
2024-08-31 14:38:02 -0300

    added to products and about pages

--- src/app/public/pages/home/home.component.ts
+++ src/app/public/pages/home/home.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { ServicesComponent } from '../../components/services/services.component';
 import { CustomizerSettingsService } from '../../../shared/components/customizer-settings/customizer-settings.service';
+import { ProductsComponent } from '../../components/products/products.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
     ServicesComponent,
+    ProductsComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
