f8641a867660ef767d42db8655850c905052e6a6
Lucas Campos <bhlucascampos@gmail.com>
2024-08-31 21:57:10 -0300

    add products

--- src/app/public/pages/products/products.component.ts
+++ src/app/public/pages/products/products.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { CustomizerSettingsService } from '../../../shared/components/customizer-settings/customizer-settings.service';
+import { MatCardModule } from '@angular/material/card';
+import { RouterLink } from '@angular/router';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSliderModule } from '@angular/material/slider';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, MatCardModule, MatCheckboxModule, MatSliderModule, FormsModule, MatButtonModule, MatIconModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
 export class ProductsComponent {
+  // Price
+  startValue = 10;
+  endValue = 4500;
 
+  // isToggled
+  isToggled = false;
+
+  constructor(
+    public themeService: CustomizerSettingsService
+  ) {
+    this.themeService.isToggled$.subscribe(isToggled => {
+      this.isToggled = isToggled;
+    });
+  }
 }
