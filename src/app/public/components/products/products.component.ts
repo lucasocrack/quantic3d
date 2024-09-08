1e1d1993dcb35146b25d6e1afdd0dce2c4eb77a8
Lucas Campos <bhlucascampos@gmail.com>
2024-09-07 21:56:22 -0300

    add products

--- src/app/public/components/products/products.component.ts
+++ src/app/public/components/products/products.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { CustomizerSettingsService } from '../../../shared/components/customizer-settings/customizer-settings.service';
 import { MatCardModule } from '@angular/material/card';
 import { RouterLink } from '@angular/router';
@@ -11,9 +12,18 @@ import { MatIconModule } from '@angular/material/icon';
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [RouterLink, MatCardModule, MatCheckboxModule, MatSliderModule, FormsModule, MatButtonModule, MatIconModule],
+  imports: [
+    CommonModule,
+    RouterLink,
+    MatCardModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    FormsModule,
+    MatButtonModule,
+    MatIconModule
+  ],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrls: ['./products.component.scss']
 })
 export class ProductsComponent {
   // Price
@@ -22,6 +32,34 @@ export class ProductsComponent {
 
   // isToggled
   isToggled = false;
+
+  products = [
+    {
+      name: 'Funkloud',
+      image: 'images/products/funkloud.jpg',
+      link: '/ecommerce-page/product-details'
+    },
+    {
+      name: 'Peter e Mary',
+      image: 'images/products/peter_e_mary.jpg',
+      link: '/ecommerce-page/product-details'
+    },
+    {
+      name: 'Luffy Gear Five',
+      image: 'images/products/luffy_gear_five.jpg',
+      link: '/ecommerce-page/product-details'
+    },
+    {
+      name: 'Gigante esmeralda, o incrível Hulk',
+      image: 'images/products/incrivel_hulk.jpg',
+      link: '/ecommerce-page/product-details'
+    },
+    {
+      name: 'Tifa Lockhart Final Fantasy VII',
+      image: 'images/products/tifa_lockhart.jpg',
+      link: '/ecommerce-page/product-details'
+    }
+  ];
 
   constructor(
     public themeService: CustomizerSettingsService
